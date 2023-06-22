ea404a6388b8e898ee3f2b5da73bd9b0b29fcd3f
utsavdas10 <97392403+utsavdas10@users.noreply.github.com>
2023-06-22 18:50:24 +0530

    mobile menu

--- src/landing/pages/home.js
+++ src/landing/pages/home.js
@@ -97,10 +97,10 @@ const Home = () => {
                 </div>
                 <div className="py-6">
                   <a
-                    href="#"
+                    href="/auth"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
-                    Log in
+                    {!auth.isLoggedIn ? 'Sign in' : 'Dashboard'}
                   </a>
                 </div>
               </div>
