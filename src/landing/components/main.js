d2fd6466f20f4013af31a8f281e8cec708afd31a
utsavdas10 <97392403+utsavdas10@users.noreply.github.com>
2023-06-24 01:20:59 +0530

    fixed button splitting

--- src/landing/components/main.js
+++ src/landing/components/main.js
@@ -8,7 +8,7 @@ const Main = () => {
     return (
         <div className="bg-white main bigbox">
       <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-        <div className="relative isolate overflow-hidden bg-slate-200 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+        <div className="main-card relative isolate overflow-hidden bg-slate-200 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
           <svg
             viewBox="0 0 1024 1024"
             className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
@@ -25,11 +25,11 @@ const Main = () => {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start clean">
               <Link to="/auth">
-              <a
+              <button
                 className="rounded-md bg-rose-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-rose-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
                 {!auth.isLoggedIn?"CREATE ACCOUNT":"DASHBOARD"}
-              </a>
+              </button>
               </Link> 
               <a href="#" className="text-sm font-semibold leading-6 text-black">
                 Learn more <span aria-hidden="true">→</span>
